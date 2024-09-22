b25cfb15160157e9af61f75468100d192526da7c
hassam009 <hasideveloper@gmail.com>
2024-09-22 23:56:01 +0500

    Create User and do authentication

--- authentication-authorization/app.js
+++ authentication-authorization/app.js
@@ -1,69 +1,78 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+const path=require('path');
+app.set("view engine", "ejs");
 app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+const userModel=require("./models/user")
 
-app.get("/", function (req, res) {
-  res.send("done");
+app.get('/',(req,res)=>{
+  res.render('index')
+})
+
+// app.get("/", function (req, res) {
+  // res.send("done");
 
   // Generate salt and hash the password
-  bcrypt.genSalt(10, function (err, salt) {
-    if (err) {
-      console.error("Error generating salt:", err);
-      return;
-    }
+  // bcrypt.genSalt(10, function (err, salt) {
+  //   if (err) {
+  //     console.error("Error generating salt:", err);
+  //     return;
+  //   }
 
-    bcrypt.hash("pololo", salt, function (err, hash) {
-      if (err) {
-        console.error("Error hashing password:", err);
-        return;
-      }
-      console.log("Generated hash:", hash);
+  //   bcrypt.hash("pololo", salt, function (err, hash) {
+  //     if (err) {
+  //       console.error("Error hashing password:", err);
+  //       return;
+  //     }
+  //     console.log("Generated hash:", hash);
 
-      // Compare the password with the hash (inside the callback)
-      bcrypt.compare("pololo", hash, function (err, result) {
-        if (err) {
-          console.error("Error comparing hash:", err);
-          return;
-        }
-        if (result) {
-          console.log("Password is correct");
-        } else {
-          console.log("Password is incorrect");
-        }
-      });
-    });
-  });
-});
+  //     // Compare the password with the hash (inside the callback)
+  //     bcrypt.compare("pololo", hash, function (err, result) {
+  //       if (err) {
+  //         console.error("Error comparing hash:", err);
+  //         return;
+  //       }
+  //       if (result) {
+  //         console.log("Password is correct");
+  //       } else {
+  //         console.log("Password is incorrect");
+  //       }
+  //     });
+  //   });
+  // });
+// });
 
-app.get("/read", function (req, res) {
-  res.send("You are on Read Page");
-});
+// app.get("/read", function (req, res) {
+//   res.send("You are on Read Page");
+// });
 
 // Route to generate JWT and send it as a cookie
-app.get("/jwt", function (req, res) {
+// app.get("/jwt", function (req, res) {
   // Generate a JWT with user email as payload
-  let token = jwt.sign({ email: "hassam12@gmail.com" }, "secret");
+  // let token = jwt.sign({ email: "hassam12@gmail.com" }, "secret");
 
   // Set the JWT as a cookie
-  res.cookie("token", token);
-  res.send("JWT token created and stored in cookie");
-});
+//   res.cookie("token", token);
+//   res.send("JWT token created and stored in cookie");
+// });
 
 // Route to verify the JWT from the cookie
-app.get("/jwtR", function (req, res) {
+// app.get("/jwtR", function (req, res) {
   // Verify the JWT token from the cookie
-  try {
-    let data = jwt.verify(req.cookies.token, "secret");
-    console.log(data); // Prints the payload (email) if the token is valid
-    res.send("JWT is valid. Data: " + JSON.stringify(data));
-  } catch (err) {
-    res.status(403).send("Invalid or expired token");
-  }
-});
+//   try {
+//     let data = jwt.verify(req.cookies.token, "secret");
+//     console.log(data); // Prints the payload (email) if the token is valid
+//     res.send("JWT is valid. Data: " + JSON.stringify(data));
+//   } catch (err) {
+//     res.status(403).send("Invalid or expired token");
+//   }
+// });
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
