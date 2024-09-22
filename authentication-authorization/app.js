edb2036b303eef9ce2c7af89cb270d98bb70c2c2
hassam009 <hasideveloper@gmail.com>
2024-09-23 01:45:31 +0500

    UserCreated

--- authentication-authorization/app.js
+++ authentication-authorization/app.js
@@ -10,10 +10,26 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 const userModel=require("./models/user")
-
+const bcrypt=require('bcrypt');
 app.get('/',(req,res)=>{
   res.render('index')
 })
+//TO CREATE USER WITH MONGODB BELOW IS THE CODE
+app.post('/create', (req,res)=>{
+  let{username, email, password,age}=req.body;
+  bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt, async(err,hash)=>{
+      let createdUser= await userModel.create({
+        username,
+        email,
+        password:hash,
+        age
+      });
+      res.send(createdUser);
+    })
+    })
+  })
+
 
 // app.get("/", function (req, res) {
   // res.send("done");
