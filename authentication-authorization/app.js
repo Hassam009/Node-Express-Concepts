6fdfe6adcccbb1a10f7d410eef246721924d7565
hassam009 <hasideveloper@gmail.com>
2024-09-27 02:46:19 +0500

    Miniproject

--- authentication-authorization/app.js
+++ authentication-authorization/app.js
@@ -1,67 +1,116 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-const path=require('path');
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
 app.set("view engine", "ejs");
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
-const userModel=require("./models/user")
-const bcrypt=require('bcrypt');
-app.get('/',(req,res)=>{
-  res.render('index')
-})
-//TO CREATE USER WITH MONGODB BELOW IS THE CODE
-app.post('/create', (req,res)=>{
-  let{username, email, password,age}=req.body;
-  bcrypt.genSalt(10,(err,salt)=>{
-    bcrypt.hash(password,salt, async(err,hash)=>{
-      let createdUser= await userModel.create({
-        username,
-        email,
-        password:hash,
-        age
-      });
-      res.send(createdUser);
-    })
-    })
-  })
+app.use(express.static(path.join(__dirname, "public")));
 
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// TO CREATE USER WITH MONGODB
+app.post("/create", async (req, res) => {
+  try {
+    let { username, email, password, age } = req.body;
+    let user = await userModel.findOne({ email });
+    if (user) return res.status(400).send("User already registered");
+
+    // Hashing password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        let createdUser = await userModel.create({
+          username,
+          email,
+          password: hash,
+          age,
+        });
+
+        let token = jwt.sign(
+          { email: createdUser.email, userId: createdUser._id },
+          "sdhkfsdf"
+        );
+        res.cookie("token", token);
+        res.send(createdUser);
+      });
+    });
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    let user = await userModel.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("User not found");
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (result) {
+        let token = jwt.sign({ user: user.email }, "sdhkfsdf");
+        res.cookie("token", token);
+        res.send("Yes, you can log in");
+      } else {
+        res.status(400).send("Incorrect password");
+      }
+    });
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
 
 // app.get("/", function (req, res) {
-  // res.send("done");
+// res.send("done");
 
-  // Generate salt and hash the password
-  // bcrypt.genSalt(10, function (err, salt) {
-  //   if (err) {
-  //     console.error("Error generating salt:", err);
-  //     return;
-  //   }
+// Generate salt and hash the password
+// bcrypt.genSalt(10, function (err, salt) {
+//   if (err) {
+//     console.error("Error generating salt:", err);
+//     return;
+//   }
 
-  //   bcrypt.hash("pololo", salt, function (err, hash) {
-  //     if (err) {
-  //       console.error("Error hashing password:", err);
-  //       return;
-  //     }
-  //     console.log("Generated hash:", hash);
+//   bcrypt.hash("pololo", salt, function (err, hash) {
+//     if (err) {
+//       console.error("Error hashing password:", err);
+//       return;
+//     }
+//     console.log("Generated hash:", hash);
 
-  //     // Compare the password with the hash (inside the callback)
-  //     bcrypt.compare("pololo", hash, function (err, result) {
-  //       if (err) {
-  //         console.error("Error comparing hash:", err);
-  //         return;
-  //       }
-  //       if (result) {
-  //         console.log("Password is correct");
-  //       } else {
-  //         console.log("Password is incorrect");
-  //       }
-  //     });
-  //   });
-  // });
+//     // Compare the password with the hash (inside the callback)
+//     bcrypt.compare("pololo", hash, function (err, result) {
+//       if (err) {
+//         console.error("Error comparing hash:", err);
+//         return;
+//       }
+//       if (result) {
+//         console.log("Password is correct");
+//       } else {
+//         console.log("Password is incorrect");
+//       }
+//     });
+//   });
+// });
 // });
 
 // app.get("/read", function (req, res) {
@@ -70,17 +119,17 @@ app.post('/create', (req,res)=>{
 
 // Route to generate JWT and send it as a cookie
 // app.get("/jwt", function (req, res) {
-  // Generate a JWT with user email as payload
-  // let token = jwt.sign({ email: "hassam12@gmail.com" }, "secret");
+// Generate a JWT with user email as payload
+// let token = jwt.sign({ email: "hassam12@gmail.com" }, "secret");
 
-  // Set the JWT as a cookie
+// Set the JWT as a cookie
 //   res.cookie("token", token);
 //   res.send("JWT token created and stored in cookie");
 // });
 
 // Route to verify the JWT from the cookie
 // app.get("/jwtR", function (req, res) {
-  // Verify the JWT token from the cookie
+// Verify the JWT token from the cookie
 //   try {
 //     let data = jwt.verify(req.cookies.token, "secret");
 //     console.log(data); // Prints the payload (email) if the token is valid
@@ -90,6 +139,6 @@ app.post('/create', (req,res)=>{
 //   }
 // });
 
-app.listen(3000, function () {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, function () {
+//   console.log("Server is running on port 3000");
+// });
--- authentication-authorization/models/user.js
+++ authentication-authorization/models/user.js
@@ -1,12 +1,14 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/authtestapp');
+// mongoose.connect('mongodb://127.0.0.1:27017/authtestapp');
+mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
 
 const userSchema=mongoose.Schema({
     username:String,
     email:String,
     password:String,
-    age:Number
+    age:Number,
+    posts:[{type:mongoose.Schema.Types.ObjectId, ref:"post"}],
 })
 
 module.exports=mongoose.model("user", userSchema);
